37a77cf090fc447c599b91d2254b242ff6f23b0a
poy410 <poy410@naver.com>
2016-05-09 11:33:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/ohdaekyoung/miniapplication/com/begentgorup/miniapplication/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.ohdaekyoung.miniapplication.com.begentgorup.miniapplication.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ohdaekyoung.miniapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
